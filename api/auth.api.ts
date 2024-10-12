53c2ddd89ea2a928035b55ed6801df1724a33aea
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2024-10-12 18:44:44 +0700

    add refresh token flow

--- api/auth.api.ts
+++ api/auth.api.ts
@@ -1,14 +1,23 @@
-import { LogInDto } from "../types/api/dto";
+import { Token } from "@/types/api/response/token.response";
+import { LogInDto, RefreshDto } from "../types/api/dto";
 import { LoginResponse } from "../types/api/response";
 import http from "../utils/http";
 
 export const AUTH_URL = {
   LOGIN: '/auth/login',
+  REFRESH: '/auth/refresh',
+  CHECK_USER_EXIST: '/auth/check-user-exist',
 };
 
 export const authAPI = {
   login(loginDto: LogInDto) {
     return http.post<LoginResponse>(AUTH_URL.LOGIN, loginDto);
+  },
+  refresh(refreshDto: RefreshDto) {
+    return http.post<Token>(AUTH_URL.REFRESH, refreshDto);
+  },
+  checkUserExist(phone: string) {
+    return http.get<boolean>(`${AUTH_URL.CHECK_USER_EXIST}/${phone}`);
   },
   // findAll(params?: FindAllOptionDto) {
   //   return http.post<OptionResponse[]>(OPTIONS_URL.FIND, params);
--- utils/http/index.ts
+++ utils/http/index.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 import { STORAGE_KEY } from "../constants/";
 // import { notificationError } from "./notification";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { authAPI } from "@/api";
 
 export class Http {
   public instance: AxiosInstance;
@@ -18,7 +19,9 @@ export class Http {
 
     this.instance.interceptors.request.use(
       async (config) => {
-        const accessToken = await AsyncStorage.getItem(STORAGE_KEY.ACCESS_TOKEN);
+        const accessToken = await AsyncStorage.getItem(
+          STORAGE_KEY.ACCESS_TOKEN
+        );
         if (accessToken) {
           config.headers.Authorization = `Bearer ${accessToken}`;
         }
@@ -28,13 +31,68 @@ export class Http {
       (error) => Promise.reject(error)
     );
 
+    let isRefreshToken = false;
+    let requestsToRefresh: any[] = [];
+
     this.instance.interceptors.response.use(
       (response) => {
         return response;
       },
       async (error) => {
+        const { config } = error;
         if (error?.response?.status === 401) {
-          return Promise.reject(error);
+          // return Promise.reject(error);
+          const refreshToken = await AsyncStorage.getItem(
+            STORAGE_KEY.REFRESH_TOKEN
+          );
+          const id = await AsyncStorage.getItem(STORAGE_KEY.ID);
+          if (!refreshToken || !id) {
+            localStorage.clear();
+            return (window.location.href = "/login");
+          }
+
+          if (!isRefreshToken) {
+            // @todo update status isRefresh to be true
+            isRefreshToken = true;
+
+            authAPI
+              .refresh({
+                id,
+                refresh_token: refreshToken,
+                is_new_refresh_token: false,
+              })
+              .then((response) => {
+                requestsToRefresh.forEach((callback) => {
+                  callback(response.data.access_token);
+                });
+                // localStorage.setItem(STORAGE_KEY.ACCESS_TOKEN, response.data.access_token);
+                // return this.instance.request(config);
+              })
+              .catch((error) => {
+                requestsToRefresh.forEach((cb) => cb(null));
+                localStorage.clear();
+                return (window.location.href = "/login");
+              })
+              .finally(() => {
+                // 5. After that, to clear all setup
+                isRefreshToken = false;
+                requestsToRefresh = [];
+              });
+          }
+
+          // 3. Setup callback to change token in headers authorization
+          return new Promise((resolve, reject) => {
+            requestsToRefresh.push(async (token: any) => {
+              if (token) {
+                await AsyncStorage.setItem(STORAGE_KEY.ACCESS_TOKEN, token);
+                // Reset access_token for another request behind
+                config.headers.Authorization = `Bearer ${token}`;
+                resolve(this.instance.request(config));
+              }
+
+              reject(error);
+            });
+          });
         }
 
         // notificationError("", error.response?.data);
